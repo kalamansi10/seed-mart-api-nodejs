94a4365a0a680df78ba1b15a546276e884f6b479
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-02 02:26:57 +0800

    Added remaining controller actions from rails counterpart

--- controllers/api/v1/shopController.js
+++ controllers/api/v1/shopController.js
@@ -1,126 +1,93 @@
-const asyncHandler = require("express-async-handler");
-const Item = require("../../../models/item");
+const Item = require('../../../models/item'); // Import Item model
+const Banner = require('../../../models/banner'); // Import Banner model
 
+// GET /api/v1/search
 exports.search = async (req, res) => {
   try {
-    // Extract query parameters
-    const keyword = req.query.keyword.toLowerCase();
-    const offset = parseInt(req.query.offset) || 0; // Convert offset to integer, default to 0 if not provided
+    const query = req.query
+    const keyword = query.keyword.toLowerCase();
+    const offset = Number(query.offset) || 0;
+    const limit = offset + 20;
 
-    // Build query to filter items based on keyword
-    const query = { tags: { $regex: new RegExp(keyword, 'i') } };
+    let itemList = Item.find({ tags: { $regex: new RegExp(keyword, 'i') } });
+    
+    if (query.filter) itemList = itemList.where(query.filter); // Add filter by category
+    if (query.minimum) itemList = itemList.where({ price: {$gte: query.minimum}}); // Minimum price
+    if (query.maximum) itemList = itemList.where({ price: {$lte: query.maximum}}); // Maximum price
 
-    // Fetch items based on query and apply pagination
-    const item_list = await Item.find(query)
-                                .limit(20)
-                                .skip(offset)
-                                .lean(); // Convert documents to plain JavaScript objects
+    itemList = itemSorter(itemList, query.sort_by);
+  
+    let item_list = await itemList.exec()
 
-    const mappedItems = item_list.map(item => {
-      return item_details(item);
-    });
-                                     
-    // Get total count of items
-    const itemCount = await Item.countDocuments(query);
+    const item_count = item_list.length; // Item count before mapping and slicing
 
-    // Send response
-    res.json({ item_list: mappedItems, item_count: itemCount});
+    // Map each item to item_details
+    item_list = item_list.map(item => itemDetails(item));
+    console.log(limit)
+    item_list = item_list.slice(offset, limit);
+
+    res.json({item_list, item_count});
   } catch (error) {
-    // Handle errors
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-// Extract details for each item
-function item_details(item) {
+// GET /api/v1/activeBanners
+exports.activeBanners = async (req, res) => {
+  try {
+    const banners = await Banner.find({ is_active: true });
+    res.json(banners);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+// GET /api/v1/getItem/:item_id - will update placeholder fields
+exports.getItem = async (req, res) => {
+  try {
+    let item = await Item.findById(req.params.item_id).lean();
+    res.json({...item, id: item._id, average_rating: 5, reviews: [], item_sold: 999});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+// GET /api/v1/itemsProperties - needs to be improved
+exports.itemsProperties = async (req, res) => {
+  try {
+    const filters = Object.keys(Item.schema.paths).filter(attribute => !["_id", "name", "price", "tags", "image_links", "createdAt", "updatedAt", "__v"].includes(attribute));
+    const properties = await Promise.all(filters.map(async attribute => {
+      return [attribute, await Item.distinct(attribute)];
+    }));
+    res.json(Object.fromEntries(properties));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+// Helper function to add sorting on query object
+function itemSorter(itemList, sort_by) {
+  switch (sort_by) {
+    case "price-lowest":
+      return itemList.sort({ price: 1 });
+    case "price-highest":
+      return itemList.sort({ price: -1 });
+    default:
+      return itemList.sort({ createdAt: -1 });
+  }
+}
+
+// Helper function to extract details for each item
+function itemDetails(item) {
   return {
     id: item._id.toString(),
     name: item.name,
     price: item.price,
     preview_image: item.image_links ? item.image_links[0] : null,
-    items_sold: 0
     // items_sold: item.orders.filter(order => order.status === 'received').length
   };
 }
-
-// class Api::V1::ShopController < ApplicationController
-//   # GET /api/v1/search
-//   def search
-//     keyword = params[:keyword].downcase
-//     offset = params[:offset]
-
-//     # Filter items based on keyword and additional filters
-//     item_list = item_sorter(item_filter(price_filter(Item)))
-//                 .where("tags LIKE ?", "%#{keyword}%")
-
-//     render json: {
-//       item_list: item_list.limit(20)
-//                           .offset(offset)
-//                           .map { |item| item_details(item) },
-//       item_count: item_list.count
-//     }
-//   end
-
-//   # GET /api/v1/active_banners
-//   def active_banners
-//     # Retrieve active banners
-//     render json: Banner.where(is_active?: true)
-//   end
-
-//   # GET /api/v1/get_item/:item_id
-//   def get_item
-//     # Retrieve details of a specific item
-//     render json: Item.get(params[:item_id])
-//   end
-
-//   # GET /api/v1/items_properties
-//   def items_properties
-//     filters = Item.column_names - ["id", "name", "price", "items_sold", "tags", "image_links", "created_at", "updated_at"]
-//     # Retrieve distinct property values for items
-//     render json: filters.map { |attribute| [attribute, Item.distinct.pluck(attribute)] }.to_h
-//   end
-
-//   private
-
-//   # Sorts item list
-//   def item_sorter(item_list)
-//     case params[:sort_by]
-//     when nil
-//       return item_list.order(created_at: :desc)
-//     when "price-lowest"
-//       return item_list.order(price: :asc)
-//     when "price-highest"
-//       return item_list.order(price: :desc)
-//     end
-//   end
-
-//   # Apply additional filters to the item list
-//   def item_filter(item_list)
-//     return item_list if params[:filter].blank?
-
-//     params[:filter].each do |k, v|
-//       item_list = item_list.where(k => v)
-//     end
-
-//     item_list
-//   end
-
-//   # Apply price range filter to the item list
-//   def price_filter(item_list)
-//     item_list = item_list.where("price >= ?", params[:minimum]) unless params[:minimum].blank?
-//     item_list = item_list.where("price <= ?", params[:maximum]) unless params[:maximum].blank?
-//     item_list
-//   end
-
-//   # Extract details for each item
-//   def item_details(item)
-//     {
-//       id: item.id,
-//       name: item.name,
-//       price: item.price,
-//       preview_image: item.image_links[0],
-//       items_sold: item.orders.where(status: 'received').count
-//     }
-//   end
-// end
--- routes/api_v1.js
+++ routes/api_v1.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const shop_controller = require("../controllers/api/v1/shopController");
 
 router.get("/search", shop_controller.search)
-// router.get("/items-properties", )
-// router.get("/active-banners", )
-// router.get("/get-item/:item_id", )
+router.get("/items-properties", shop_controller.itemsProperties)
+router.get("/active-banners", shop_controller.activeBanners)
+router.get("/get-item/:item_id", shop_controller.getItem)
 
 module.exports = router;
