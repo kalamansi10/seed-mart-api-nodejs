4ef613b3285f1aba973df2fa7424b087b4b6e6ca
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-05 08:50:36 +0800

    Added userInfo  virtual

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Define the schema for the user
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  encrypted_password: { type: String, required: true },
+  password_digest: { type: String, required: true },
   // reset_password_token: { type: String, unique: true },
   // reset_password_sent_at: { type: Date },
   // remember_created_at: { type: Date },
@@ -11,6 +11,18 @@ const userSchema = new mongoose.Schema({
   gender: { type: String },
   birthday: { type: Date },
 }, { timestamps: true });
+
+userSchema.virtual('userInfo').get(function() {
+  return {
+    email: this.email,
+    name: this.name,
+    gender: this.gender,
+    birthday: this.birthday,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt
+    // Add other fields as needed
+  };
+});
 
 // Create a model based on the schema
 const User = mongoose.model('User', userSchema);
