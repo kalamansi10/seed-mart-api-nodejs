498769b19e5e4be651bc487477c9601c4c7868ef
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-13 09:07:44 +0800

    Added id virtual for front end compatibility

--- models/item.js
+++ models/item.js
@@ -11,7 +11,7 @@ const itemSchema = new mongoose.Schema({
   planting_method: String,
   package_size: String,
   price: { type: Number, required: true },
-  tags: String,
+  tags: { type: String, required: true },
   image_links: { 
     type: [String], 
     default: [
@@ -22,6 +22,14 @@ const itemSchema = new mongoose.Schema({
     ]
   }
 }, { timestamps: true });
+
+itemSchema.virtual("id").get(function () {
+  return this._id.toHexString()
+});
+
+itemSchema.set("toJSON", {
+  virtuals: true
+})
 
 // Create a model based on the schema
 const Item = mongoose.model('Item', itemSchema);
