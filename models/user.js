059cb4bebdab33ca68f6240dc9cc58ff04075491
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-13 09:07:21 +0800

    Applied prettier

--- models/user.js
+++ models/user.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the schema for the user
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  passwordDigest: { type: String, required: true },
-  // reset_password_token: { type: String, unique: true },
-  // reset_password_sent_at: { type: Date },
-  rememberMeToken: { type: String },
-  name: { type: String },
-  gender: { type: String },
-  birthday: { type: Date },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    passwordDigest: { type: String, required: true },
+    // reset_password_token: { type: String, unique: true },
+    // reset_password_sent_at: { type: Date },
+    rememberMeToken: { type: String },
+    name: { type: String },
+    gender: { type: String },
+    birthday: { type: Date },
+  },
+  { timestamps: true }
+);
 
-userSchema.virtual('info').get(function() {
+userSchema.virtual("info").get(function () {
   return {
     id: this._id,
     email: this.email,
@@ -20,12 +23,12 @@ userSchema.virtual('info').get(function() {
     gender: this.gender,
     birthday: this.birthday,
     createdAt: this.createdAt,
-    updatedAt: this.updatedAt
+    updatedAt: this.updatedAt,
     // Add other fields as needed
   };
 });
 
 // Create a model based on the schema
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
