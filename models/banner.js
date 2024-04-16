116a188d5fb25fb1f4a145f37204906a49ae444c
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-16 14:52:34 +0800

    Added id virtual to banner model

--- models/banner.js
+++ models/banner.js
@@ -1,14 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the schema for the banner
-const bannerSchema = new mongoose.Schema({
-  banner_name: { type: String, required: true },
-  image_link: { type: String, required: true },
-  banner_link: { type: String, required: true },
-  is_active: { type: Boolean, default: true },
-}, { timestamps: true });
+const bannerSchema = new mongoose.Schema(
+  {
+    banner_name: { type: String, required: true },
+    image_link: { type: String, required: true },
+    banner_link: { type: String, required: true },
+    is_active: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+bannerSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+bannerSchema.set("toJSON", {
+  virtuals: true,
+});
 
 // Create a model based on the schema
-const Banner = mongoose.model('Banner', bannerSchema);
+const Banner = mongoose.model("Banner", bannerSchema);
 
 module.exports = Banner;
