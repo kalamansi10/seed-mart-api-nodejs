7db5a71267943556f2b8a19595f56a3738c3e19c
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-19 10:24:33 +0800

    Update routing to match with rails version of API

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,39 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
-const shop_controller = require("../controllers/api/v1/shopController");
+const item_controller = require("../controllers/api/v1/itemController");
 const cart_controller = require("../controllers/api/v1/cartController");
-const user_controller = require("../controllers/api/v1/userController");
+const account_controller = require("../controllers/api/v1/accountController");
 const order_controller = require("../controllers/api/v1/orderController");
+const review_controller = require("../controllers/api/v1/reviewController");
+const misc_controller = require("../controllers/api/v1/miscController");
 
 // Shop item/s queries
-router.get("/search", shop_controller.search)
-router.get("/items-properties", shop_controller.itemsProperties)
-router.get("/active-banners", shop_controller.activeBanners)
-router.get("/get-item/:item_id", shop_controller.getItem)
+router.get("/item/search", item_controller.searchItems)
+router.get("/item/:item_id", item_controller.getItem)
 
 // User carted items & options
-router.get("/get-cart", cart_controller.getCart)
-router.get("/get-for-checkout", cart_controller.getForCheckout)
-router.post("/add-to-cart", cart_controller.addToCart)
-router.put("/update-carted-amount/:carted_id/:amount", cart_controller.updateCartedAmount)
-router.put("/update-checkout-status/:carted_id/:is_for_checkout", cart_controller.updateCheckoutStatus)
-router.delete("/remove-from-cart/:carted_id", cart_controller.removeFromCart)
+router.get("/cart", cart_controller.getCart)
+router.get("/cart/for-checkout", cart_controller.getForCheckout)
+router.post("/cart", cart_controller.addToCart)
+router.put("/cart/update-amount/:carted_id/:amount", cart_controller.updateCartedAmount)
+router.put("/cart/update-status/:carted_id/:is_for_checkout", cart_controller.updateCheckoutStatus)
+router.delete("/cart/:carted_id", cart_controller.removeFromCart)
 
 // User shipping address & options
-router.get("/get-shipping-addresses", user_controller.getShippingAddress)
-router.post("/add-shipping-address", user_controller.addShippingAddress)
-router.put("/update-shipping-address", user_controller.updateShippingAddress)
-router.delete("/remove-shipping-address/:shipping_address_id", user_controller.removeShippingAddress)
+router.get("/account/shipping-address/list", account_controller.getShippingAddressList)
+router.post("/account/shipping-address", account_controller.addShippingAddress)
+router.put("/account/shipping-address", account_controller.updateShippingAddress)
+router.delete("/account/shipping-address/:shipping_address_id", account_controller.removeShippingAddress)
 
 // get "/get-payment-methods", to: "user#get_payment_methods"
 // post "/add-payment-method", to: "user#add_payment_method"
 // put "/update-payment-method", to: "user#update_payment_method"
 // delete "/remove-payment-method/:payment_method_id", to: "user#remove_payment_method"
 
-// router.get("/get-orders", user_controller.getOrders)
-// router.get("/get-order", user_controller.getOrder)
-
+// Order processing and fetching
+router.get("/order/list", order_controller.getOrderList)
+router.get("/order/:reference_id", order_controller.getOrder)
 router.post("/order", order_controller.processOrder)
+
+// Order review actions
+router.get("/review/list/:item_id", review_controller.getReviewList)
+router.post("/review", review_controller.addReview)
+router.put("/review", review_controller.editReview)
+router.get("/review/:review_id", review_controller.deleteReview)
+
+// Misc endpoints
+router.get("/misc/active-banners", misc_controller.getActiveBanners)
+router.get("/misc/item-properties", misc_controller.getItemProperties)
 
 module.exports = router;
